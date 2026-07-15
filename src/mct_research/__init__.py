4d27dfb1b31cf80bbe934bc004b0485eee5bfed7
Kajin-0 <28634576+Kajin-0@users.noreply.github.com>
2026-07-15 16:35:43 -0700

    Expose two-coupling Kane model API

--- src/mct_research/__init__.py
+++ src/mct_research/__init__.py
@@ -1,15 +1,37 @@
 """Executable analytical models for HgCdTe research."""
 
-from .kane8 import ALPHA_EV_A2, KaneParameters, hamiltonian, time_reversal_residual
-from .projection import closure_residual, design_diagnostics, fit_parameters, parameter_templates
+from .kane8 import (
+    ALPHA_EV_A2,
+    ExtendedKaneParameters,
+    KaneParameters,
+    hamiltonian,
+    hamiltonian_two_p,
+    time_reversal_residual,
+    time_reversal_residual_two_p,
+)
+from .projection import (
+    closure_residual,
+    design_diagnostics,
+    extended_closure_residual,
+    extended_design_diagnostics,
+    fit_extended_parameters,
+    fit_parameters,
+    parameter_templates,
+)
 
 __all__ = [
     "ALPHA_EV_A2",
+    "ExtendedKaneParameters",
     "KaneParameters",
     "closure_residual",
     "design_diagnostics",
+    "extended_closure_residual",
+    "extended_design_diagnostics",
+    "fit_extended_parameters",
     "fit_parameters",
     "hamiltonian",
+    "hamiltonian_two_p",
     "parameter_templates",
     "time_reversal_residual",
+    "time_reversal_residual_two_p",
 ]
